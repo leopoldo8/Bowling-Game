89b40fb2b8abace2c66bfb7ba030c9c8f04ebf8b
Rodrigo Haertel <rodrigo.haertel@nodo.cc>
2019-05-08 17:55:12 -0300

    feature: frame and score interface implemented
    and also fixed some lint's warnings.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="about">
+    <div class="bkg_logo">
+      <img alt="logo" src="../assets/logo.png" width="150">
+    </div>
     <h1>About page</h1>
     <p>This is a "pre-prototype" of Bowling game. A only-training app for now.</p>
     <p>I expect to evolve much more this project in the course of time, so stay tuned!</p>
   </div>
 </template>
+
+<style lang="sass" scoped>
+.bkg_logo
+    width: 150px
+    height: 150px
+    border-radius: 100%
+    background-color: #49648e
+    margin: 0 auto
+    display: flex
+    justify-content: center
+    align-items: center
+
+</style>
